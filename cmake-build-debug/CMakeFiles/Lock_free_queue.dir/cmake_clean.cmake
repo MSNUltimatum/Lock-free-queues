0ef935ac33049470ed031de1a6c4ec612d28417b
Ultimatum <egrsar2000@gmail.com>
2020-10-25 17:58:02 +0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/Lock_free_queue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lock_free_queue.dir/main.c.obj"
+  "Lock_free_queue.exe"
+  "Lock_free_queue.exe.manifest"
+  "Lock_free_queue.pdb"
+  "libLock_free_queue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lock_free_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Lock_free_queue.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lock_free_queue.
+# This may be replaced when dependencies are built.
